acf65ec8c91278b5ceef5415cd4e4f412a903c0d
thrw-wy-blancc <88911683+thrw-wy-blancc@users.noreply.github.com>
2021-09-30 04:14:19 +0300

    added custom-range-slider

---
+++ script.js
@@ -0,0 +1,25 @@
+const range = document.getElementById("range");
+
+range.addEventListener("input", (e) => {
+    const value = +e.target.value;
+    const label = e.target.nextElementSibling;
+
+    const rangeWidth = getComputedStyle(e.target).getPropertyValue("width");
+
+    const labelWidth = getComputedStyle(label).getPropertyValue("width");
+
+    const rangeNumWidth = +rangeWidth.substring(0, rangeWidth.length - 2);
+    const labelNumWidth = +labelWidth.substring(0, labelWidth.length - 2);
+
+    const max = +e.target.max;
+    const min = +e.target.min;
+
+    const left = value * (rangeNumWidth / max) - labelNumWidth / 2 + scale(value, min, max, 10, -10);
+
+    label.style.left = `${left}px`;
+    label.innerHTML = value;
+});
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
